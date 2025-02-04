393139b0c72c4050fd2d0ee93b91176edc48b705
SoSo12121212 <yo789yo25@gmail.com>
2025-02-04 17:07:54 +0900

    App

---
+++ my-app/src/App2.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Library from "./2-jsx/Library";
+
+/**
+ * 
+ *  return(
+  <MainApp>
+    <Header/>
+    <Nav/>
+    <Main >
+      <section></section>
+    </Main>
+  </MainApp>
+ )
+ */
+function App() {
+  return <Library />;
+}
+export default App;
---
+++ my-app/src/App4.js
@@ -0,0 +1,8 @@
+import React from "react";
+//import * as E from " ./4-props/Props";
+import * as ex from "./4-props/Ex";
+
+function App() {
+  return <ex.Props1 />;
+}
+export default App;
